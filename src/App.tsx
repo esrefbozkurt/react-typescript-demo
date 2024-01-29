73141534f58b1d23873f1b7557ce08acb15aea46
Esref <esref.bozkurt@gmx.de>
2024-01-29 20:04:44 +0100

    install axios for API call, add useEffect and call API, safe users data in useState and return with array map method as ul, add type for user information, destructure needed user Info,add style for h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import { FC } from "react";
+import axios from "axios";
+import { FC, useEffect, useState } from "react";
 
 interface AppProps {
   title: string;
 }
 
-const App: FC<AppProps> = () => {
-  return <div>App</div>;
+interface Name {
+  first: string;
+  last: string;
+}
+
+interface Login {
+  uuid: string;
+}
+
+interface Users {
+  name: Name;
+  login: Login;
+  email: string;
+}
+
+const App: FC<AppProps> = ({ title }) => {
+  const [users, setUsers] = useState<Users[]>([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://randomuser.me/api/?results=10"
+        );
+        console.log(data);
+        setUsers(data?.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>
+        {users.map(({ login, name, email }) => {
+          return (
+            <li key={login.uuid}>
+              <div>
+                Name: {name.first} {name.last}
+              </div>
+              <div>Email: {email}</div>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
